286b14c0ce24c4c34d763169d8bc9701123eb166
as-amaan-109 <official.amaansyedateeq@gamil.com>
2026-02-11 08:49:04 +0530

    Some Chages in code

---
+++ src/app/api/product/delete-review/route.ts
@@ -0,0 +1,56 @@
+import { NextResponse } from "next/server";
+import connectDB from "@/lib/db";
+import Product from "@/models/product.model";
+
+export async function DELETE(req: Request) {
+  try {
+    await connectDB();
+
+    const body = await req.json();
+    console.log("Delete review body:", body);
+
+    const { productId, reviewId } = body;
+
+    if (!productId || !reviewId) {
+      return NextResponse.json(
+        { message: "productId or reviewId missing" },
+        { status: 400 }
+      );
+    }
+
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return NextResponse.json(
+        { message: "Product not found" },
+        { status: 404 }
+      );
+    }
+
+    const initialLength = product.reviews.length;
+
+    product.reviews = product.reviews.filter(
+      (r: any) => r._id.toString() !== reviewId.toString()
+    );
+
+    if (product.reviews.length === initialLength) {
+      return NextResponse.json(
+        { message: "Review not found" },
+        { status: 404 }
+      );
+    }
+
+    await product.save();
+
+    return NextResponse.json(
+      { message: "Review deleted successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("DELETE REVIEW ERROR:", error);
+    return NextResponse.json(
+      { message: "Server error" },
+      { status: 500 }
+    );
+  }
+}
--- src/app/view-product/[id]/page.tsx
+++ src/app/view-product/[id]/page.tsx
@@ -144,7 +144,7 @@ const avgRating =
 
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-900 px-4 py-10">
+    <div className="min-h-screen bg-linear-to-br from-gray-900 via-black to-gray-900 px-4 py-10">
       <div className="max-w-6xl mx-auto">
 
         {/* ================== ✅ TOP SECTION (LEFT + RIGHT) ================== */}
